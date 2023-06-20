e4db8f98653dab164bb8189e119017ff0d76c105
janc18 <josecla011@gmail.com>
2023-06-20 12:27:52 -0600

    chore: se corrige el estilo de codigo

--- Codigo/Firmware/Main_board_HID/ESP32-S3/components/include/Usb_communication.h
+++ Codigo/Firmware/Main_board_HID/ESP32-S3/components/include/Usb_communication.h
@@ -4,12 +4,10 @@
 #include "class/hid/hid_device.h"
 #include "tinyusb.h"
 
-#define TUSB_DESC_TOTAL_LEN                                                    \
-    (TUD_CONFIG_DESC_LEN + CFG_TUD_HID * TUD_HID_DESC_LEN)
+#define TUSB_DESC_TOTAL_LEN (TUD_CONFIG_DESC_LEN + CFG_TUD_HID * TUD_HID_DESC_LEN)
 
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
-                               hid_report_type_t report_type, uint8_t *buffer,
-                               uint16_t reqlen);
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type,
+                               uint8_t *buffer, uint16_t reqlen);
 
 uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance);
 void usb_initialization(void);
