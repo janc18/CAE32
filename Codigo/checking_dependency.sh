b03e5ad022746a5671a86bfc54e88f0852dcfaa7
janc18 <josecla011@gmail.com>
2023-12-19 16:35:27 -0600

    update: added a package installed an not installed matrix

--- Codigo/checking_dependency.sh
+++ Codigo/checking_dependency.sh
@@ -2,10 +2,14 @@
 
 echo -e "\e[1mDependency\t Status\e[0m"
 echo -e "\t\t|---------"
-for cmd in cmake gcc make; do 
+for cmd in cmake gcc make pkgconf; do 
   if ! command -v $cmd &> /dev/null; then
     echo -e "$cmd \t\t \e[31mNot installed\e[0m"
+    Packages_not_installed+=($cmd)
+    exit 1
   else
     echo -e "$cmd \t\t \e[32mInstalled\e[0m"
+    Packages_installed+=($cmd)
   fi 
 done
+
