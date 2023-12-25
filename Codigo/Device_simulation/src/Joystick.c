db4f69e43481700b04369f16b247b0118fbff0b2
janc18 <josecla011@gmail.com>
2023-12-24 21:54:44 -0600

    refactor: included shared library and added the new device descriptor from the shared library and cleaning the rdesc comments

--- Codigo/Device_simulation/src/Joystick.c
+++ Codigo/Device_simulation/src/Joystick.c
@@ -47,7 +47,7 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include "device_desc_pedals.h"
 /*
  * HID Report Desciptor
  * We emulate a basic 3 button mouse with wheel and 3 keyboard LEDs. This is
@@ -112,8 +112,6 @@
 static unsigned char rdesc[] = {
     0x05, 0x01,                         /* USAGE_PAGE (Generic Desktop) */
     0x09, 0x04,                         /* USAGE (Joystick) */
-                                        //	0xa1, 0x01,	/* COLLECTION (Application) */
-                                        //	0x09, 0x01,		/* USAGE (Pointer) */
     0xa1, 0x01,                         /* COLLECTION (Physical) */
     0x85, 0x01,                         /* REPORT_ID (1) */
     0x05, 0x09,                         /* USAGE_PAGE (Button) */
@@ -124,37 +122,19 @@ static unsigned char rdesc[] = {
     0x95, 0x08,                         /* REPORT_COUNT (3) */
     0x75, 0x01,                         /* REPORT_SIZE (1) */
     0x81, 0x02,                         /* INPUT (Data,Var,Abs) */
-                                        //	0x95, 0x01,			/* REPORT_COUNT (1) */
-                                        //	0x75, 0x05,			/* REPORT_SIZE (5) */
-                                        //	0x81, 0x01,			/* INPUT (Cnst,Var,Abs) */
-                                        //	0x85, 0x02,			/* REPORT_ID (1) */
     0x05, 0x01,                         /* USAGE_PAGE (Generic Desktop) */
     0x09, 0x30,                         /* USAGE (X) */
     0x09, 0x31,                         /* USAGE (Y) */
-    0x09, 0x32,                         /* USAGE (WHEEL) */
-    0x09, 0x33, 0x15, 0x81,             /* LOGICAL_MINIMUM (-127) */
+    0x09, 0x32,                         /* USAGE (Z) */
+    0x09, 0x33,                         /* USAGE (WHEEL) */
+    0x15, 0x81,                         /* LOGICAL_MINIMUM (-127) */
     0x25, 0x7f,                         /* LOGICAL_MAXIMUM (127) */
-    0x35, 0xc9, 0x45, 0xc8, 0x75, 0x08, /* REPORT_SIZE (8) */
-    0x95, 0x04,                         /* REPORT_COUNT (3) */
+    0x35, 0xc9,                         /* PHYSICAL_MINIMUM ()*/
+    0x45, 0xc8,                         /* PHYSICAL_MAXIMUM ()*/
+    0x75, 0x08,                         /* REPORT_SIZE (8) */
+    0x95, 0x04,                         /* REPORT_COUNT (4) */
     0x81, 0x02,                         /* INPUT (Data,Var,Rel) */
     0xc0                                /* END_COLLECTION */
-                                        //	0xc0,		/* END_COLLECTION */
-                                        //	0x05, 0x01,	/* USAGE_PAGE (Generic Desktop) */
-                                        //	0x09, 0x06,	/* USAGE (Keyboard) */
-                                        //	0xa1, 0x01,	/* COLLECTION (Application) */
-                                        //	0x85, 0x02,		/* REPORT_ID (2) */
-                                        //	0x05, 0x08,		/* USAGE_PAGE (Led) */
-                                        //	0x1a, 0x01,		/* USAGE_MINIMUM (1) */
-                                        //	0x29, 0x03,		/* USAGE_MAXIMUM (3) */
-                                        //	0x15, 0x00,		/* LOGICAL_MINIMUM (0) */
-                                        //	0x25, 0x01,		/* LOGICAL_MAXIMUM (1) */
-                                        //	0x95, 0x03,		/* REPORT_COUNT (3) */
-                                        //	0x75, 0x01,		/* REPORT_SIZE (1) */
-                                        //	0x91, 0x02,		/* Output (Data,Var,Abs) */
-                                        //	0x95, 0x01,		/* REPORT_COUNT (1) */
-                                        //	0x75, 0x05,		/* REPORT_SIZE (5) */
-                                        //	0x91, 0x01,		/* Output (Cnst,Var,Abs) */
-                                        //	0xc0,		/* END_COLLECTION */
 };
 
 static int uhid_write(int fd, const struct uhid_event *ev) {
@@ -180,8 +160,8 @@ static int create(int fd) {
   strcpy((char *)ev.u.create2.name, "CAE32 Steering Wheel");
   // ev.u.create.rd_data = rdesc;
   // ev.u.create2.rd_data[sizeof(rdesc)];
-  memcpy(ev.u.create2.rd_data, rdesc, sizeof(rdesc));
-  ev.u.create2.rd_size = sizeof(rdesc);
+  memcpy(ev.u.create2.rd_data, Pedals_descriptor, sizeof(Pedals_descriptor));
+  ev.u.create2.rd_size = sizeof(Pedals_descriptor);
   ev.u.create2.bus = BUS_USB;
   ev.u.create2.vendor = 0x15d9;
   ev.u.create2.product = 0x0a37;
