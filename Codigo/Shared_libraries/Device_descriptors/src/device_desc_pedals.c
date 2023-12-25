350c1e0317389d7f91a88918d8556a4c29192ae1
janc18 <josecla011@gmail.com>
2023-12-24 22:00:17 -0600

    add: pedal device descriptor for testing purposes

--- Codigo/Shared_libraries/Device_descriptors/src/device_desc_pedals.c
+++ Codigo/Shared_libraries/Device_descriptors/src/device_desc_pedals.c
@@ -13,7 +13,7 @@
  * 
  * - Terminal console for real-time configuration changes or command transmission.
  * 
- * .. note::
+ * .. note:
  *    Due to the PCB's high flexibility in configuration, the device descriptor
  *    may vary for each specific case.
  *    For this particular device descriptor, the standard configuration will be
@@ -26,4 +26,18 @@
 
 #include "device_desc_pedals.h"
 
-
+char Pedals_descriptor [25]={
+    0x05, 0x01, /* USAGE_PAGE (Generic Desktop Page) */
+    0x09, 0x04, /* USAGE (Joystick) */
+    0xA1, 0x01, /* COLLECTION (Physical) */
+    0x85, 0x01, /* REPORT_ID (1) */
+    0x09, 0x30, /* USAGE (Accelerator)*/
+    0x09, 0x31, /* USAGE (Brake)*/
+    0x09, 0x32, /* USAGE (Clutch)*/
+    0x15, 0x00, /* Logical_minimum (0) */
+    0x25, 0xFE, /* Logical_maximum (255) */
+    0x95, 0x03, /* report_count (3) */
+    0x75, 0x03, /* report_size (1) */
+    0x81, 0x02, /* input (data,var,abs) */
+    0xc0        /* end_collection */
+};
