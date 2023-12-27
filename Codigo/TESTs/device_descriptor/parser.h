b9428dccfaeaf1bd477fa006ee151b89b8243579
janc18 <josecla011@gmail.com>
2023-12-26 22:27:40 -0600

    update: added get tokens and find word functions to the header file

--- Codigo/TESTs/device_descriptor/parser.h
+++ Codigo/TESTs/device_descriptor/parser.h
@@ -3,4 +3,6 @@
 char *file_to_string(char *file_path);
 //int find_number_objects(char *string_file);
 int find_number_of_lines(char *string_file);
+int find_word(char *string_file, char *word, int offset);
+char **get_line_tokens(char *line);
 #endif // !PARSER_H
