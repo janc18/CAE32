31c519ddab2d9cf80ab3b4e0075a9b287dd5c73f
janc18 <josecla011@gmail.com>
2023-12-27 21:14:35 -0600

    update: added function prototypes

--- Codigo/TESTs/device_descriptor/parser.h
+++ Codigo/TESTs/device_descriptor/parser.h
@@ -1,8 +1,9 @@
 #ifndef PARSER_H
 #define PARSER_H
 char *file_to_string(char *file_path);
-//int find_number_objects(char *string_file);
+// int find_number_objects(char *string_file);
 int find_number_of_lines(char *string_file);
-int find_word(char *string_file, char *word, int offset);
+int **find_word(char *string_file, char *word, int offset);
 char **get_line_tokens(char *line);
+int find_object(char *string_file);
 #endif // !PARSER_H
