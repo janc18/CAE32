3d1a8a64b4ec5ad628b5e51de35de2fe49c6e080
janc18 <josecla011@gmail.com>
2023-12-29 13:27:47 -0600

    update: added structs to save tokens, and removed prototypes functions

--- Codigo/TESTs/device_descriptor/parser.h
+++ Codigo/TESTs/device_descriptor/parser.h
@@ -1,9 +1,22 @@
 #ifndef PARSER_H
 #define PARSER_H
+
+#include <stdbool.h>
+
+typedef struct line_token{
+  char *parameter;
+  char *value;
+}line_token;
+
+typedef struct token_descriptor {
+  int cursor_offset;
+  bool is_word_found;
+  line_token tokenize;
+} token_descriptor;
+
+
 char *file_to_string(char *file_path);
-// int find_number_objects(char *string_file);
 int find_number_of_lines(char *string_file);
-int **find_word(char *string_file, char *word, int offset);
-char **get_line_tokens(char *line);
-int find_object(char *string_file);
+line_token *get_line_tokens(char *line);
+int free_line_token(line_token *tokens);
 #endif // !PARSER_H
