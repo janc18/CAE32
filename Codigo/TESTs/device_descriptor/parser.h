95c64ddf2c928cc066aa2feab948f2502b34ce01
janc18 <josecla011@gmail.com>
2023-12-30 20:53:36 -0600

    chore: updated function prototype of get_file_tokens and added a function to fre get_file_tokens memory

--- Codigo/TESTs/device_descriptor/parser.h
+++ Codigo/TESTs/device_descriptor/parser.h
@@ -19,5 +19,6 @@ char *file_to_string(char *file_path);
 int find_number_of_lines(char *string_file);
 line_token *get_line_tokens(char *line);
 int free_line_token(line_token *tokens);
-int get_file_tokens(char *string_file);
+line_token **get_file_tokens(char *string_file);
+int free_memory_tokens(line_token **tokens, int number_of_lines);
 #endif // !PARSER_H
