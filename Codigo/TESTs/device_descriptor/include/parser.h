642ecc728779deda486dfeb08cfa83b20e332dcb
janc18 <josecla011@gmail.com>
2024-01-03 00:33:18 -0600

    delete: Struct that wasn't used

--- Codigo/TESTs/device_descriptor/include/parser.h
+++ Codigo/TESTs/device_descriptor/include/parser.h
@@ -3,17 +3,11 @@
 
 #include <stdbool.h>
 
-extern const char Keywords[][30]; 
+extern const char Keywords[][30];
 typedef struct line_token {
   char *parameter;
   char *value;
 } line_token;
-
-typedef struct token_descriptor {
-  int cursor_offset;
-  bool is_word_found;
-  line_token tokenize;
-} token_descriptor;
 
 char *get_each_line_of_file_string(char *string_file, int *offset);
 char *file_to_string(char *file_path);
@@ -22,4 +16,6 @@ line_token *get_line_tokens(char *line);
 int free_line_token(line_token *tokens);
 line_token **get_file_tokens(char *string_file);
 int free_memory_tokens(line_token **tokens, int number_of_lines);
+int remove_extra_spaces(char *line);
+int number_of_delimiter(char *line, char delimiter);
 #endif // !PARSER_H
