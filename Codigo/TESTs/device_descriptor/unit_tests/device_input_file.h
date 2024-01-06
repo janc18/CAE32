58a6b284311f02c3a065b8d15c9fc0239370edfe
janc18 <josecla011@gmail.com>
2024-01-05 19:02:08 -0600

    rename: device_input_file.cpp -> device_input_file.h

---
+++ Codigo/TESTs/device_descriptor/unit_tests/device_input_file.h
@@ -0,0 +1,4 @@
+#ifndef DEVICE_INPUT_FILE_CPP
+#define DEVICE_INPUT_FILE_CPP
+char string_file_c[] = "Start : CAE31 Pedals\nAxis : 3\nSize_bits : 8\nLogical_Minimum: -127\nLogical_Maximum : 127\nPhysical_Minumum : 0\nPhysical_Maximum : 255\nEnd : CAE32 Pedals\n\nStart: CAE32 Shifter\nButtons : 8\nSize_bits : 1\nLogical_Minimum : 0\nLogical_Maximum : 1\nPhysical_Minumum : 0\nPhysical_Maximum : 1\nEnd: CAE32 Shifter\n\nStart : CAE32 Steering Wheel\nButtons : 15\nSize_bits : 1\nAxis : 1\nSize_bits : 16\nLogical_Minimum : 6\nLogical_Maximum : 1\nPhysical_Minumum : 0\nPhysical_Maximum : 1\nEnd : CAE32 Steering Wheel";
+#endif // !DEVICE_INPUT_FILE_CPP
