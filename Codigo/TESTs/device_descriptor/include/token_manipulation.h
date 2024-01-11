346ccdf608e27094488a41228173c6e310421710
janc18 <josecla011@gmail.com>
2024-01-10 23:25:12 -0600

    refactor: function prototypes

--- Codigo/TESTs/device_descriptor/include/token_manipulation.h
+++ Codigo/TESTs/device_descriptor/include/token_manipulation.h
@@ -8,8 +8,9 @@ int end;
 }object_index;
 
 int free_memory_object(object_index **objects);
-object_index *find_object(line_token **token_file, int number_of_lines, int start_index);
-object_index **get_objects_index(line_token **token_file, int number_of_lines);
+object_index *find_object(lines_tokenize *token_line,int start_index);
+// object_index **get_objects_index(lines_tokenize *token_lines);
+object_index *search_start_and_end_index(lines_tokenize *token_line,int start_index);
 int search_for_word(line_token *token_file,const char *parameter, int index);
 int print_contents_of_n_object(line_token **token_file, object_index content_index);
 int verify_object(line_token **token_file,object_index index);
