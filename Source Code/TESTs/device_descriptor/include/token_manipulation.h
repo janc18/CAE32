4383c08344b7bafa7f0ebdf16575300c0c6700cc
janc18 <josecla011@gmail.com>
2024-02-04 21:52:44 -0600

    chore: update function prototype

--- Source Code/TESTs/device_descriptor/include/token_manipulation.h
+++ Source Code/TESTs/device_descriptor/include/token_manipulation.h
@@ -12,7 +12,7 @@ object_index *find_object(lines_tokenize *token_line,int start_index);
 int verify_object(lines_tokenize *token_file, object_index index);
 // object_index **get_objects_index(lines_tokenize *token_lines);
 object_index *search_start_and_end_index(lines_tokenize *token_line,int start_index);
+void print_contents_of_n_object(lines_tokenize *token_file, object_index *content_index);
 int search_for_word(line_token *token_file,const char *parameter, int index);
-int print_contents_of_n_object(line_token **token_file, object_index content_index);
 char *token_error(int error_result);
 #endif // !TOKEN_MANIPULATION_H
