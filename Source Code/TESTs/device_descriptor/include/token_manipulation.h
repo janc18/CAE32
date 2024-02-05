fb3ac6776e21f19d481af30390b20ca775d1a4e9
janc18 <josecla011@gmail.com>
2024-02-05 00:57:22 -0600

    chore: added function prototype

--- Source Code/TESTs/device_descriptor/include/token_manipulation.h
+++ Source Code/TESTs/device_descriptor/include/token_manipulation.h
@@ -15,4 +15,5 @@ object_index *search_start_and_end_index(lines_tokenize *token_line,int start_in
 void print_contents_of_n_object(lines_tokenize *token_file, object_index *content_index);
 int search_for_word(line_token *token_file,const char *parameter, int index);
 char *token_error(int error_result);
+int find_number_of_objects(lines_tokenize *token_file);
 #endif // !TOKEN_MANIPULATION_H
