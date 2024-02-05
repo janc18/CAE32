8a50f8389df3c81c18d87b47ac9963c4ba2f5802
janc18 <josecla011@gmail.com>
2024-02-04 20:52:37 -0600

    chore: update function prototype and added a new one (token_error)

--- Source Code/TESTs/device_descriptor/include/token_manipulation.h
+++ Source Code/TESTs/device_descriptor/include/token_manipulation.h
@@ -9,9 +9,10 @@ int end;
 
 int free_memory_object(object_index **objects);
 object_index *find_object(lines_tokenize *token_line,int start_index);
+int verify_object(lines_tokenize *token_file, object_index index);
 // object_index **get_objects_index(lines_tokenize *token_lines);
 object_index *search_start_and_end_index(lines_tokenize *token_line,int start_index);
 int search_for_word(line_token *token_file,const char *parameter, int index);
 int print_contents_of_n_object(line_token **token_file, object_index content_index);
-int verify_object(line_token **token_file,object_index index);
+char *token_error(int error_result);
 #endif // !TOKEN_MANIPULATION_H
