f3e1422c8f2b9d84820e1a780b08143ee7991a02
janc18 <josecla011@gmail.com>
2024-03-04 13:34:01 -0600

    chore: added functions comments

--- Source Code/TESTs/device_descriptor/src/read_file_descriptor.c
+++ Source Code/TESTs/device_descriptor/src/read_file_descriptor.c
@@ -1,3 +1,16 @@
+/**
+ * @file read_file_descriptor.c
+ *
+ * @brief Get all the hid device related information
+
+ * - To get all the information about the hid device, it can be acomplish using
+ *   linux libraries like hidraw, getting for example the next values:
+ *    - Device name
+ *    - Device descriptor
+ *    - Hid features
+ *  All ioctl commands are explain in the next link:https://www.kernel.org/doc/html/latest/hid/hidraw.html
+ *
+ */
 #include <linux/hidraw.h>
 #include <linux/input.h>
 #include <linux/types.h>
@@ -18,6 +31,14 @@
 unsigned char usuage_id_axis_values[6] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35};
 char hid_path[] = "/dev/hidraw";
 
+/**
+ * @brief Check if a hexadecimal value exist in an unsigned char array
+ *
+ *  @param char Value to search
+ *  @param unsigned char[] Array to search the value
+ *  @param int Array size
+ *  @return bool true if found or false if didn't
+ */
 bool exist_in_array(unsigned char value, unsigned char array_to_compare[], int array_size) {
   for (int i = 0; i < array_size; i++) {
     if (array_to_compare[i] == value) {
@@ -27,6 +48,18 @@ bool exist_in_array(unsigned char value, unsigned char array_to_compare[], int a
   return false;
 }
 
+/**
+ * @brief Generate an allocated string with the hid path 
+ *
+ * @param char* Hid path to be concatenate
+ * @param int Number to concatenate with the hid path
+ *
+ * Example: /dev/hidraw 0
+ *          ^^^^^^^^^^^ ^
+ *          |         | |
+ *          path         number
+ * @return char* Allocated string with the hid pad generated
+ */
 char *generate_hid_path(char *path, int number_device) {
   int buffersize = 30;
   char *p_buffer = malloc(buffersize * sizeof(char));
@@ -34,6 +67,17 @@ char *generate_hid_path(char *path, int number_device) {
   return p_buffer;
 }
 
+/**
+ * @brief Get the file descriptor of a given device name
+ *
+ * @param char* Hid path, Example(/dev/hidraw1)
+ * @param char* Device name to be compared with the hid device name
+ *
+ * @return int 
+ *  -1 Unable to open device or an error occour when getting the name
+ *  -2 Compared name it's the correct one
+ *  >0 If it was find, return the actual file descriptor number
+ */
 int get_fd(char *path_device, char *compare_device_name) {
   int fd, res;
   char buffer[50] = {0};
@@ -66,6 +110,15 @@ int get_fd(char *path_device, char *compare_device_name) {
   return -1;
 }
 
+
+/**
+ * @brief Search in all hid device connected for the given device name
+ *
+ * @param char* Hid path, Example(/dev/hidraw1)
+ * @param int* File descriptor number
+ *
+ * @return bool true when it's found or false if din't
+ */ 
 bool search_device(char *path_device, int *fd) {
   char *newpath;
   int iteration = 0;
