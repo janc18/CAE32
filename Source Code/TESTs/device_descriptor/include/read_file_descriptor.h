9819cc361b72a76963a0861525b9db20bd6d5377
janc18 <josecla011@gmail.com>
2024-03-09 12:53:21 -0600

    fix: changed prototype function(search_device) now return a char * with the path of the actual searching device

--- Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
+++ Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
@@ -4,6 +4,6 @@
 bool exist_in_array(unsigned char value, unsigned char array_to_compare[], int array_size);
 char *generate_hid_path(char *path, int number_device);
 int get_fd(char *path_device, char *compare_device_name);
-bool search_device(char *path_device, int *fd);
+char *search_device(char *path_device);
 struct hidraw_report_descriptor *get_report_descriptor(char *path_device);
 #endif
