383131568fbf56039744ea3c448c080e88a9dccd
janc18 <josecla011@gmail.com>
2024-03-09 13:02:20 -0600

    add: an example of how get informartion from the report descriptor

--- Source Code/TESTs/device_descriptor/device_descriptor_tester.c
+++ Source Code/TESTs/device_descriptor/device_descriptor_tester.c
@@ -10,7 +10,9 @@
  * - it require an input file to automate a little bit the test process
  */
 
+#include "read_file_descriptor.h"
 #include "struct_manipulation.h"
+#include <linux/hidraw.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,8 +20,17 @@ int main(int argc, char *argv[]) {
   devices_handle *devices;
   devices = get_all_information_from_device_c_32(argv[1]);
   char *buttons = get_feature_value_from_device_c32_file(1, devices, "Buttons");
-  if (buttons != NULL)
-    printf("Quantity of buttons %s\n", buttons);
+  char *object_name_1 = get_object_name(2, devices);
+  if (object_name_1 != NULL) {
+    printf("%s\n", object_name_1);
+    char *hid_path = search_device(object_name_1);
+    if (hid_path != NULL) {
+      struct hidraw_report_descriptor *rpt_des;
+      rpt_des = get_report_descriptor(hid_path);
+      free(hid_path);
+      printf("%xxh\n",rpt_des->value[0]);
+    }
+  }
   free_all_memory(devices);
   return EXIT_SUCCESS;
 }
