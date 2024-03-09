9c65ad9279072b9b4d6addd989ec59b0a5b67447
janc18 <josecla011@gmail.com>
2024-03-08 20:34:40 -0600

    add: get_report_descriptor function

--- Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
+++ Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
@@ -5,4 +5,5 @@ bool exist_in_array(unsigned char value, unsigned char array_to_compare[], int a
 char *generate_hid_path(char *path, int number_device);
 int get_fd(char *path_device, char *compare_device_name);
 bool search_device(char *path_device, int *fd);
+struct hidraw_report_descriptor *get_report_descriptor(char *path_device);
 #endif
