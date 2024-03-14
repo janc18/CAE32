fea2e18116f28c92bb63c9ace0f3f36cc1f68f2d
janc18 <josecla011@gmail.com>
2024-03-13 23:24:16 -0600

    chore: change generate_hid_path name to generate_path

--- Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
+++ Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
@@ -2,7 +2,7 @@
 #define READ_FILE_DESCRIPTOR
 #include <stdbool.h>
 bool exist_in_array(unsigned char value, unsigned char array_to_compare[], int array_size);
-char *generate_hid_path(char *path, int number_device);
+char *generate_path(char *path, int number_device);
 int get_fd(char *path_device, char *compare_device_name);
 char *search_device(char *path_device);
 struct hidraw_report_descriptor *get_report_descriptor(char *path_device);
