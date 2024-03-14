6e380f41557f3cace6a5f7fbfc87d210c2347cb8
janc18 <josecla011@gmail.com>
2024-03-13 23:28:45 -0600

    chore: added example to read events from an device given from a text file

--- Source Code/TESTs/device_descriptor/device_descriptor_tester.c
+++ Source Code/TESTs/device_descriptor/device_descriptor_tester.c
@@ -12,25 +12,25 @@
 
 #include "read_file_descriptor.h"
 #include "struct_manipulation.h"
+#include <fcntl.h>
 #include <linux/hidraw.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "read_event.h"
 
 int main(int argc, char *argv[]) {
+
+  // Check if it has sudo permission to read the report descriptor
   devices_handle *devices;
   devices = get_all_information_from_device_c_32(argv[1]);
   char *buttons = get_feature_value_from_device_c32_file(1, devices, "Buttons");
   char *object_name_1 = get_object_name(2, devices);
-  if (object_name_1 != NULL) {
-    printf("%s\n", object_name_1);
-    char *hid_path = search_device(object_name_1);
-    if (hid_path != NULL) {
-      struct hidraw_report_descriptor *rpt_des;
-      rpt_des = get_report_descriptor(hid_path);
-      free(hid_path);
-      printf("%xxh\n",rpt_des->value[0]);
-    }
-  }
+  print_data(buttons);
+
+  char *event_path=get_event_path(object_name_1);
+  printf("The path is:%s\nReading events\n",event_path);
+  read_events(event_path);
   free_all_memory(devices);
   return EXIT_SUCCESS;
 }
