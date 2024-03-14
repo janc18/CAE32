9e5a61a0e9dcc9802b578ea1832d84870b501a76
janc18 <josecla011@gmail.com>
2024-03-13 23:26:24 -0600

    chore: add print_data function

--- Source Code/TESTs/device_descriptor/include/struct_manipulation.h
+++ Source Code/TESTs/device_descriptor/include/struct_manipulation.h
@@ -18,4 +18,5 @@ devices_handle *get_all_information_from_device_c_32(char *file_path) ;
 void free_all_memory(devices_handle *p_devices);
 char *get_feature_value_from_device_c32_file(int object_number, devices_handle *devices, char *feature);
 char *get_object_name(int object_number,devices_handle *devices);
+void print_data(char *data2print);
 #endif
