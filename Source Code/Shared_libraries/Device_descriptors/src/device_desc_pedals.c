2f136ab8227d63a86160f47911e48d675ea63c50
janc18 <josecla011@gmail.com>
2024-03-16 11:19:20 -0600

    update: Pedals report descriptor

--- Source Code/Shared_libraries/Device_descriptors/src/device_desc_pedals.c
+++ Source Code/Shared_libraries/Device_descriptors/src/device_desc_pedals.c
@@ -26,18 +26,30 @@
 
 #include "device_desc_pedals.h"
 
-char Pedals_descriptor [25]={
-    0x05, 0x01, /* USAGE_PAGE (Generic Desktop Page) */
-    0x09, 0x04, /* USAGE (Joystick) */
-    0xA1, 0x01, /* COLLECTION (Physical) */
-    0x85, 0x01, /* REPORT_ID (1) */
-    0x09, 0x30, /* USAGE (Accelerator)*/
-    0x09, 0x31, /* USAGE (Brake)*/
-    0x09, 0x32, /* USAGE (Clutch)*/
-    0x15, 0x00, /* Logical_minimum (0) */
-    0x25, 0xFE, /* Logical_maximum (255) */
-    0x95, 0x03, /* report_count (3) */
-    0x75, 0x03, /* report_size (1) */
-    0x81, 0x02, /* input (data,var,abs) */
-    0xc0        /* end_collection */
+unsigned char Pedals_descriptor[49] = {
+    0x05, 0x01,                         /* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x04,                         /* USAGE (Joystick) */
+    0xa1, 0x01,                         /* COLLECTION (Physical) */
+    0x85, 0x01,                         /* REPORT_ID (1) */
+    0x05, 0x09,                         /* USAGE_PAGE (Button) */
+    0x19, 0x01,                         /* USAGE_MINIMUM (Button 1) */
+    0x29, 0x08,                         /* USAGE_MAXIMUM (Button 3) */
+    0x15, 0x00,                         /* LOGICAL_MINIMUM (0) */
+    0x25, 0x01,                         /* LOGICAL_MAXIMUM (1) */
+    0x95, 0x08,                         /* REPORT_COUNT (3) */
+    0x75, 0x01,                         /* REPORT_SIZE (1) */
+    0x81, 0x02,                         /* INPUT (Data,Var,Abs) */
+    0x05, 0x01,                         /* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x30,                         /* USAGE (X) */
+    0x09, 0x31,                         /* USAGE (Y) */
+    0x09, 0x32,                         /* USAGE (Z) */
+    0x09, 0x33,                         /* USAGE (WHEEL) */
+    0x15, 0x00,                         /* LOGICAL_MINIMUM (0) */
+    0x25, 0xFF,                         /* LOGICAL_MAXIMUM (255) */
+    0x35, 0xc9,                         /* PHYSICAL_MINIMUM ()*/
+    0x45, 0xc8,                         /* PHYSICAL_MAXIMUM ()*/
+    0x75, 0x08,                         /* REPORT_SIZE (8) */
+    0x95, 0x04,                         /* REPORT_COUNT (4) */
+    0x81, 0x02,                         /* INPUT (Data,Var,Rel) */
+    0xc0                                /* END_COLLECTION */
 };
