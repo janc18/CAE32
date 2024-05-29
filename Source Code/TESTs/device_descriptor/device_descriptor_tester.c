6bbc6e08cea636bf3448569791ee5d7d489042bb
jose <josecla011@gmail.com>
2024-05-29 13:38:37 -0600

    fix: when an input file isn't provided

--- Source Code/TESTs/device_descriptor/device_descriptor_tester.c
+++ Source Code/TESTs/device_descriptor/device_descriptor_tester.c
@@ -10,6 +10,7 @@
  * - it require an input file to automate a little bit the test process
  */
 
+#include "read_event.h"
 #include "read_file_descriptor.h"
 #include "struct_manipulation.h"
 #include <fcntl.h>
@@ -17,19 +18,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "read_event.h"
 
 int main(int argc, char *argv[]) {
 
   // Check if it has sudo permission to read the report descriptor
+  if (argv[1] == NULL) { // Checking if a path was given
+    fprintf(stderr, "ERROR: Doesn't give any input path\n");
+    return EXIT_FAILURE;
+  }
+
   devices_handle *devices;
   devices = get_all_information_from_device_c_32(argv[1]);
+
+  if (devices == NULL) {
+    return EXIT_FAILURE;
+  }
+
   char *buttons = get_feature_value_from_device_c32_file(1, devices, "Buttons");
   char *object_name_1 = get_object_name(2, devices);
   print_data(buttons);
 
-  char *event_path=get_event_path(object_name_1);
-  printf("The path is:%s\nReading events\n",event_path);
+  char *event_path = get_event_path(object_name_1);
+  printf("The path is:%s\nReading events\n", event_path);
   read_events(event_path);
   free_all_memory(devices);
   return EXIT_SUCCESS;
