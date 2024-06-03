9ad2f5637241841d3258cfe1ddaf7a559f06c7f5
jose <josecla011@gmail.com>
2024-06-03 12:17:10 -0600

    chore: added prototypes functions

--- Source Code/TESTs/device_descriptor/include/read_event.h
+++ Source Code/TESTs/device_descriptor/include/read_event.h
@@ -1,5 +1,8 @@
 #ifndef READ_EVENT_H
 #define READ_EVENT_H
+#include <stdbool.h>
 char *getEventPath(char *name_to_compare);
 void readEvents(char *path_event);
+bool hasSudoPermissions();
+bool continueWithoutSudoPermissions();
 #endif // !READ_EVENT_h
