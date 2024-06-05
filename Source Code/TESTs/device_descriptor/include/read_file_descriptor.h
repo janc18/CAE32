bf3ed2a9f1ddc7c4e2f2f3fb803124e4f7b4a57e
jose <josecla011@gmail.com>
2024-06-05 10:10:12 -0600

    chore: added a extern variable

--- Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
+++ Source Code/TESTs/device_descriptor/include/read_file_descriptor.h
@@ -1,6 +1,7 @@
 #ifndef READ_FILE_DESCRIPTOR
 #define READ_FILE_DESCRIPTOR
 #include <stdbool.h>
+extern unsigned char usage_id_axis_values[6];
 bool existInArray(unsigned char value, unsigned char array_to_compare[], int array_size);
 char *generatePath(char *path, int number_device);
 int getFd(char *path_device, char *compare_device_name);
