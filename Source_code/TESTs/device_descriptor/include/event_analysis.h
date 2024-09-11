230edb68ac8f401bd0c2f42e2d04df34d314a367
janc18 <josecla011@gmail.com>
2024-09-10 23:00:14 -0600

    chore: added prototypes fuctions and struct declaration

--- Source_code/TESTs/device_descriptor/include/event_analysis.h
+++ Source_code/TESTs/device_descriptor/include/event_analysis.h
@@ -1,4 +1,12 @@
 #ifndef EVENT_ANALYSIS_H
 #define EVENT_ANALYSIS_H
 
+#include "read_event.h"
+int getNumberOfEvents(events *head);
+
+typedef struct eventCharacteristics{
+  int eventValue[10];
+  char eventName[30];
+}eventCharacteristics;
+
 #endif // !EVENT_ANALYSIS_H
