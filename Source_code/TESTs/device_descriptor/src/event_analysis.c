76164b68f3ccfd5ada27979fa4acd7b8093e5d52
janc18 <josecla011@gmail.com>
2024-09-10 23:03:48 -0600

    added: function to get all the diffents events from a device

--- Source_code/TESTs/device_descriptor/src/event_analysis.c
+++ Source_code/TESTs/device_descriptor/src/event_analysis.c
@@ -1,3 +1,29 @@
 #include "event_analysis.h"
+#include "read_event.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+void generateDataSet(events *head, eventCharacteristics *event) {
+  // Allocating memory
+  eventCharacteristics *peventcharacteristics = calloc(sizeof(eventCharacteristics), 1);
+}
 
+int getNumberOfEvents(events *head) {
+  int numberOfEvents = 0;
+  events *current = head;
+  
+  if(head == NULL){
+    printf("(arg)Head is null\n");
+    return -1;
+  }
+  
+  while (current != NULL) {
+    numberOfEvents++;
+    current = current->siguiente;
+  }
+  return numberOfEvents;
+}
+
+int appendValue(events head){
+//
+}
