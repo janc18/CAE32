fa2bfe4845ca2065a6cb0b5a66724e1a74862f2a
Jose Agustin <josecla011@gmail.com>
2024-12-15 20:54:11 -0600

    fixed: paths for the GUI setup

--- Source_code/Interfaz_grafica/Interfaz_gtk/setup.sh
+++ Source_code/Interfaz_grafica/Interfaz_gtk/setup.sh
@@ -13,10 +13,10 @@ for dir in ${ListOf_directories[@]}; do
 done
 
 # Move all the required directories of the GUI
-cp ./Source_code/Interfaz_grafica/Interfaz_gtk/build/executable $Install_directory${ListOf_directories[4]}/exec -r #path of executable file
-cp ./Source_code/Interfaz_grafica/Interfaz_gtk/src_images $Install_directory${ListOf_directories[4]} -r
-cp ./Source_code/Interfaz_grafica/Interfaz_gtk/gladeFiles $Install_directory${ListOf_directories[4]} -r 
-cp ./Source_code/Desktop_files/start_GUI.sh $Install_directory${ListOf_directories[4]}/exec 
+cp ../../Interfaz_grafica/Interfaz_gtk/build/executable $Install_directory${ListOf_directories[4]}/exec -r #path of executable file
+cp ../../Interfaz_grafica/Interfaz_gtk/src_images $Install_directory${ListOf_directories[4]} -r
+cp ../../Interfaz_grafica/Interfaz_gtk/gladeFiles $Install_directory${ListOf_directories[4]} -r 
+cp ../../Desktop_files/start_GUI.sh $Install_directory${ListOf_directories[4]}/exec 
 
 chmod +x $Install_directory/GUI/exec/executable
 chmod +x $Install_directory/GUI/exec/start_GUI.sh
